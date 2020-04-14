b6db7ccb9a845eb88feee734183efd4d8e91a200
minikiller <minikiller@qq.com>
2020-04-14 18:33:08 +0800

    test

--- test/test_telemetry.py
+++ test/test_telemetry.py
@@ -1,7 +1,11 @@
+import sys
+sys.path.append("..")
 import logger
+_logger = logger.get_logger(__name__)
 
-def hello1():
-    _logger=logger.get_logger(__name__)
+
+def test_hello1():
+
     client_data = "12.12,325.40,83.71,15.52,38.63,76.51,193.03,258.50,258.70#13.11,331.50,83.46,16.38,40.52,79.33,196.36,258.30,258.50#14.08,337.10,83.63,17.21,42.36,85.14,211.88,258.70,258.50#15.10,342.70,83.08,18.18,44.35,89.91,221.76,258.70,258.60"
     groups = client_data.split("#")
     for index, group in enumerate(groups):
@@ -10,8 +14,10 @@ def hello1():
         _logger.info(values)
         # if values[0] == "data":
 
+
 if __name__ == "__main__":
-    hello1()
+    test_hello1()
+
 
 def hello(first, second):
     """ Description
