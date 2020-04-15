60eae994c20a5a408110de77d6f61817d1e82323
minikiller <minikiller@qq.com>
2020-04-15 10:38:25 +0800

    change

--- redis_wrapper.py
+++ redis_wrapper.py
@@ -13,7 +13,7 @@ class RedisWrapper(object):
         self.__dict__ = self.shared_state
 
     def redis_connect(self):
-        redis_server_conf = REDIS_SERVER_CONF['servers']
+        redis_server_conf = REDIS_SERVER_CONF
         connection_pool = redis.ConnectionPool(host=redis_server_conf['HOST'], port=redis_server_conf['PORT'],
                                                db = redis_server_conf['DATABASE'])
         return redis.StrictRedis(connection_pool = connection_pool)
